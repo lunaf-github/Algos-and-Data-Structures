dea24a148f39cf6e83d97fe26c370235e877e542
Fernando <lunaf91@gmail.com>
2021-07-23 19:44:32 -0700

    Added buttom-up recusion solution to leetcode 104

--- src/com/fern/MaximumDepthOfBinaryTree-104-BinaryTree-Recursion.java
+++ src/com/fern/MaximumDepthOfBinaryTree-104-BinaryTree-Recursion.java
@@ -13,9 +13,25 @@
  *     }
  * }
  */
+// class Solution {
+//     public int maxDepth(TreeNode root) {
+//         if(root == null) return 0;
+//         return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+//     }
+// }
+
 class Solution {
+
     public int maxDepth(TreeNode root) {
-        if(root == null) return 0;
-        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+        return helper (root, 0);
     }
+
+    private int helper(TreeNode root, int depth){
+        if(root == null) return depth;
+        int left = helper(root.left, depth + 1);
+        int right = helper(root.right, depth + 1);
+        depth = Math.max(left, right);
+        return depth;
+    }
+
 }
