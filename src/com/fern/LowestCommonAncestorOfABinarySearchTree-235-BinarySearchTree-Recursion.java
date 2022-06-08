088dff7e2c8eac456424078c7e8619f54a0a6cf8
Fernando <lunaf91@gmail.com>
2022-06-08 16:54:55 -0700

    Updated challange 235

--- src/com/fern/LowestCommonAncestorOfABinarySearchTree-235-BinarySearchTree-Recursion.java
+++ src/com/fern/LowestCommonAncestorOfABinarySearchTree-235-BinarySearchTree-Recursion.java
@@ -10,11 +10,12 @@
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root.val >= q.val && root.val <= p.val || root.val >= p.val && root.val <= q.val) return root;
-        if(root.val >= q.val && root.val >= p.val){
-            return lowestCommonAncestor(root.left, p, q);
-        }else{
-            return lowestCommonAncestor(root.right, p , q);
+        TreeNode curr = root;
+
+        while(true){
+            if(p.val > curr.val && q.val > curr.val) curr= curr.right;
+            else if (p.val < curr.val && q.val < curr.val) curr = curr.left;
+            else return curr;
         }
 
     }
