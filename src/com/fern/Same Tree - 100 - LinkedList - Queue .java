281fe11926b28beb0f8fd6dc9886bacec2c592d0
Fernando <lunaf91@gmail.com>
2022-06-08 11:06:37 -0700

    Improved solution for challange 100

--- src/com/fern/Same Tree - 100 - LinkedList - Queue .java
+++ src/com/fern/Same Tree - 100 - LinkedList - Queue .java
@@ -13,44 +13,15 @@
  *     }
  * }
  */
+//DFS - Recursion
+// time complexity: O(p + q) = O(n);
+
 class Solution {
-    public boolean check(TreeNode p, TreeNode q){
-        if(p == null & q == null) return true;
-        if(p == null || p == null) return false;
-        if(p.val != q.val) return false;
-        return true;
-    }
-
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        Queue <TreeNode> pQueue = new LinkedList<>();
-        Queue <TreeNode> qQueue = new LinkedList<>();
-
         if(p == null && q == null) return true;
         if(p == null || q == null) return false;
+        if(p.val != q.val) return false;
 
-        pQueue.offer(p);
-        qQueue.offer(q);
-
-
-        while(!pQueue.isEmpty() && !qQueue.isEmpty()){
-            TreeNode tempq = qQueue.poll();
-            TreeNode tempp = pQueue.poll();
-
-            if(tempq.val != tempp.val) return false;
-
-            if(tempq.left != null) qQueue.offer(tempq.left);
-            if(tempp.left != null) pQueue.offer(tempp.left);
-
-            if(pQueue.size() != qQueue.size()) return false;
-
-            if(tempq.right != null) qQueue.offer(tempq.right);
-            if(tempp.right != null) pQueue.offer(tempp.right);
-
-            if(pQueue.size() != qQueue.size()) return false;
-
-        }
-
-        return pQueue.size() == qQueue.size();
-
+        return isSameTree(p.left , q.left) && isSameTree(p.right, q.right);
     }
 }
