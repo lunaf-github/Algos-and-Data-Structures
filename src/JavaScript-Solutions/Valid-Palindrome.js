b1ea3388a10b3bc7f4b66058b014cf0644de5982
lunaf-github <lunaf91@gmail.com>
2022-12-01 14:30:01 -0800

    add regex explaination

--- src/JavaScript-Solutions/Valid-Palindrome.js
+++ src/JavaScript-Solutions/Valid-Palindrome.js
@@ -1,6 +1,11 @@
 const isPalindrome = (s) => {
 
  s = s.toLowerCase();
+
+ // in regex, characters inside the [] are characters that we want to match with
+ // if we add a ^ in the beginning inside the [], it converts it to letters we don't want 
+ // to match with. So, in the case below, we want to match any letter except a-z and 0-9. 
+ 
  s = s.replace(/[^a-z0-9]/g,'');
 
  let l = 0;
