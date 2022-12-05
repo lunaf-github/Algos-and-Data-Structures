a40af078c4b5dfa9c5e1b1232f9f34805492fb2b
lunaf-github <lunaf91@gmail.com>
2022-12-05 12:31:45 -0800

    update Best Time to Buy and sell Stock challange

--- src/Java-Solutions/121-Best-Time-to-Buy-and-Sell-Stock.java
+++ src/Java-Solutions/121-Best-Time-to-Buy-and-Sell-Stock.java
@@ -1,15 +1,34 @@
+/*
+Keep in mind:
+
+We can't have negative profit
+To get the max profit, we need the smallest buy price and highest sell price
+
+if we get a negative provit, this means that the buying price is too high. So, need to
+change our buying price to the sell price, which at the same time is the lowest value we found.
+
+Have something to keep track of the max profit.
+
+TC: O(n)
+SC: O(1)
+
+*/
+
+import java.util.Scanner;
 class Solution {
-    public int maxProfit(int[] prices) {
-        int left = 0;
-        // int minPrice = prices[0];
+    public static int maxProfit(int[] prices) {
         int maxProfit = 0;
+        int buy = 0;
 
-        for(int right = 1; right < prices.length; right++){
-            int curProfit = prices[right] - prices[left];
-            maxProfit = Math.max(maxProfit, curProfit);
-            if(prices[right] < prices[left]) left = right;
+        for(int sell = 1; sell < prices.length; sell++){
+            int curProfit = prices[sell] - prices[buy];
+            if(curProfit < 0){
+                buy = sell;
+            }else{
+                maxProfit = Math.max(maxProfit , curProfit);
+            }
         }
-
         return maxProfit;
     }
+
 }
