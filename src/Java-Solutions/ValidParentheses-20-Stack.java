683ac920939b4bdaa6ebab36ebaddd9a39876672
lunaf-github <lunaf91@gmail.com>
2022-12-05 12:56:51 -0800

    Add solution explaination

--- src/Java-Solutions/ValidParentheses-20-Stack.java
+++ src/Java-Solutions/ValidParentheses-20-Stack.java
@@ -1,3 +1,13 @@
+/*
+TC: O(n)
+SC: O(1)
+inner most parentheses will match with a closing parenthesis first.
+So, this means that we have LIFO and a stack is suitable for this problem.
+
+({)} - is not valud input because the parenthesis does not follow the parenthesis rules.
+
+ */
+
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack();
