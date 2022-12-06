b7104b2350a33789953a49ec07bb657f0c2a4b00
lunaf-github <lunaf91@gmail.com>
2022-12-05 16:07:56 -0800

    update solution

--- src/Java-Solutions/KthLargestElementInAStream-703-BinarySearchTree-Recursion.java
+++ src/Java-Solutions/KthLargestElementInAStream-703-BinarySearchTree-Recursion.java
@@ -1,51 +1,22 @@
 class KthLargest {
-    int k;
-    TreeNode root;
+    int size;
+    PriorityQueue<Integer> minheap;
+
     public KthLargest(int k, int[] nums) {
-        this.k = k;
-        for(int num: nums) root = add(root, num);
-
-    }
-
-    public TreeNode add(TreeNode root, int val){
-        if (root == null) return new TreeNode(val);
-        root.count++;
-        if(val > root.val) root.right = add(root.right, val);
-        else root.left = add(root.left, val);
-        return root;
+        this.size = k;
+        this.minheap = new PriorityQueue<>();
+        for(int num : nums){
+            minheap.add(num);
+            if(minheap.size() > size) minheap.poll();
+        }
     }
 
     public int add(int val) {
-        root = add(root,val);
-        return kLargest();
+        minheap.add(val);
+        if(minheap.size() > size) minheap.poll();
+        return minheap.peek();
     }
-
-    public int kLargest(){
-        int count = k;
-        TreeNode walker = root;
-
-        while(count > 0){
-            int pos = 1 + (walker.right != null? walker.right.count : 0);
-            if(count == pos) break;
-            if(count < pos) walker = walker.right;
-            if(count > pos) {
-                count -= pos;
-                walker = walker.left;
-            }
-        }
-
-        return walker.val;
-    }
-
-
-    class TreeNode {
-        int count = 1;
-        int val;
-        TreeNode right;
-        TreeNode left;
-        public TreeNode(int v){val = v;}
-    }
-
+}
 
 }
 
