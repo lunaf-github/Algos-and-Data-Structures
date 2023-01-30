c8aa7a35e9df0a6b14a70bf338f4ff30d70ff7d5
lunaf-github <lunaf91@gmail.com>
2023-01-30 12:04:22 -0800

    Add JS solution

---
+++ src/JavaScript-Solutions/1328-Break-a-Palindrome.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} palindrome
+ * @return {string}
+ */
+
+
+var breakPalindrome = function(palindrome) {
+ const charArr = palindrome.split('');
+ const n = charArr.length;
+ 
+ for(let i = 0; i < Math.floor(n/2); i++){
+     if(charArr[i] !== 'a'){
+         charArr[i] = 'a';
+         return charArr.join('');
+     } 
+ }
+ 
+ charArr[charArr.length - 1] = 'b';
+ 
+ return (charArr.length <=  1) ? '' : charArr.join('');
+};
