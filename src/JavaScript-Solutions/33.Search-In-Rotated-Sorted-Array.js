90e6d27aa7a3ac69e2253900b240df7b78bef778
lunaf-github <lunaf91@gmail.com>
2023-03-13 11:02:35 -0700

    Add JS solution

---
+++ src/JavaScript-Solutions/33.Search-In-Rotated-Sorted-Array.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+
+var search = function(nums, target) {
+ var left = 0;
+ var right = nums.length - 1;
+ 
+ while (left <= right) {
+     let mid = left + Math.floor((right - left)/2);
+     
+     if (nums[mid] === target) return mid;
+     
+     if (nums[mid] >= nums[left]) {
+         if (target > nums[mid] || target < nums[left]) {
+             left = mid + 1;
+         } else {
+             right = mid - 1;
+         }
+     } else {
+         if (target < nums[mid] || target > nums[right]) {
+             right = mid - 1;
+         } else {
+             left = mid + 1;
+         }
+     }
+ }
+ 
+ return -1;
+};
+
+
