ca4944015def3ede47e20fe90ae854915724347e
Fernando <lunaf91@gmail.com>
2023-10-24 10:38:51 -0700

    Update solution

--- src/JavaScript-Solutions/435-Non-overlapping-Intervals.js
+++ src/JavaScript-Solutions/435-Non-overlapping-Intervals.js
@@ -3,26 +3,23 @@
  * @return {number}
  */
 
-var eraseOverlapIntervals = function(intervals) {
- const start = 0;
- const end = 1;
- let removeCount = 0;
- 
- intervals.sort((a,b) => a[0] - b[0]);
+// Greedy, get the smallest ending of two overlapping intervals
+// Smallest ending has better chance of overlapping with more intervals
 
- let prev = 0;
- 
- for(let next = 1; next < intervals.length; next++){
-     if(intervals[prev][end] > intervals[next][start]){
-         removeCount++;
-         if(intervals[prev][end] > intervals[next][end]){
-             prev = next;
-         }
-     }else{
-         prev = next;
-     }
-     
- }
- 
- return removeCount;
+var eraseOverlapIntervals = function(intervals) {
+    intervals.sort((a, b) => a[0] - b[0]);
+    let output = 0;
+    let prevEnd = intervals[0][1];
+
+    for (let i = 1; i < intervals.length; i++) {
+        const [curStart, curEnd] = intervals[i];
+        if (curStart < prevEnd) {
+            output++;
+            prevEnd = Math.min(curEnd, prevEnd);
+        } else {
+            prevEnd = curEnd;
+        }
+    }
+
+    return output;
 };
