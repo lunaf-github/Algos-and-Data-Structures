d679763017e33c7e7fffa877a1d3f8deec9052d0
Fernando <lunaf91@gmail.com>
2024-03-11 11:01:44 -0700

    update solution

--- src/JavaScript-Solutions/155-Min-Stack.js
+++ src/JavaScript-Solutions/155-Min-Stack.js
@@ -36,7 +36,7 @@ Explanation
 */
 
 var MinStack = function() {
- this.stack = [];
+  this.stack = [];
 };
 
 /** 
@@ -44,32 +44,37 @@ var MinStack = function() {
 * @return {void}
 */
 MinStack.prototype.push = function(val) {
- let min = val;
- if(this.stack.length > 0){
-     min = Math.min(val, this.stack[this.stack.length - 1][1]);
- }
- this.stack.push([val, min]);
+  let min = val;
+
+  if (this.stack.length > 0) {
+      const top = this.stack[this.stack.length - 1];
+      min = Math.min(min, top.min);
+  }
+
+  this.stack.push({ val, min });
 };
 
 /**
 * @return {void}
 */
 MinStack.prototype.pop = function() {
- this.stack.pop()
+  this.stack.pop();
 };
 
 /**
 * @return {number}
 */
 MinStack.prototype.top = function() {
- return this.stack[this.stack.length - 1][0];
+  const top = this.stack[this.stack.length - 1];
+  return top.val;
 };
 
 /**
 * @return {number}
 */
 MinStack.prototype.getMin = function() {
- return this.stack[this.stack.length - 1][1];
+  const top = this.stack[this.stack.length - 1];
+  return top.min;
 };
 
 /** 
