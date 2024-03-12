16f1c81f2d9f3e992f3fea01fe0d4a259051de6c
Fernando <lunaf91@gmail.com>
2024-03-12 10:38:23 -0700

    update solution

--- src/JavaScript-Solutions/875-Koko-Eating-Bananas.js
+++ src/JavaScript-Solutions/875-Koko-Eating-Bananas.js
@@ -40,36 +40,42 @@ Constraints:
 // O(nlog(max(p)))
 
 var minEatingSpeed = function(piles, h) {
- //function to calculate the time to eat banana at k pace
- function calculateTime(k){
-     let t = 0;        
-     for(const pile of piles){
-         t += Math.ceil(pile/k);
-     }
-     return t;
- }
- 
- 
- // calculate largest banan pile, O(n)
- let l = 1
- let r = Math.max(...piles);
- 
- let speed = r;
- // binary search ( k = 1 to Largest Pile size) O(logn)
- while(l <= r){
-     const mid = l +  Math.floor((r - l)/2);
-     //calculate time to eat bananas O(n)
-     const t = calculateTime(mid);
+  let minK = 0;
+  let maxK = Math.max(...piles);
+  let res = maxK;
 
-     if(t <= h){
+  while (minK <= maxK) {
+      const midK = minK + Math.floor((maxK - minK) / 2);
+      const eatingTime = computeEatingTime(midK);
 
-         speed = Math.min(speed, mid)
-         r = mid - 1;
-     }else{
-         l = mid + 1
-     }  
- }
- 
- return speed;
-};  
+      if (eatingTime <= h) {
+          res = Math.min(midK, res)
+          maxK = midK - 1;
+      } else {
+          minK = midK + 1;
+      }
+  }
+
+  return res;
+
+  // ********* 
+  function computeEatingTime(k) {
+      let hours = 0;
+      piles.forEach(pile => {
+          hours += Math.ceil(pile / k);
+      });
+      return hours;
+  }
+};
+
+
+/*
+  If interviewer wants me to manually compute max of piles
+
+  let maxK = piles.reduce((acc, cur) => {
+      return Math.max(cur, acc);
+  });
+
+
+*/
 
