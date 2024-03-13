89aeb7c8e92f52e7f2dbdb55c7c8a10f6225ec12
Fernando <lunaf91@gmail.com>
2024-03-13 13:21:11 -0700

    update solution

--- src/Java-Solutions/74-Search-a-2D-Matrix.java
+++ src/Java-Solutions/74-Search-a-2D-Matrix.java
@@ -3,41 +3,47 @@
 
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int matrixWidth = matrix[0].length;
-        int matrixHeight = matrix.length;
-        int targetRow = 0;
-        int topRow = 0;
-        int botRow = matrixHeight - 1;
+        final int MATRIX_HEIGHT = matrix.length;
+        final int MATRIX_WIDTH = matrix[0].length;
 
-        while(topRow <= botRow){
-            int midRow = topRow + (botRow-topRow)/2;
+        int top = 0;
+        int bottom = MATRIX_HEIGHT - 1;
 
-            if(target > matrix[midRow][matrixWidth - 1]) topRow = midRow + 1;
-            else if (target < matrix[midRow][0]) botRow = midRow - 1;
-            else {
-                targetRow = midRow;
-                break;
+        while (top <= bottom) {
+            int mid = top + (bottom - top) / 2;
+
+            if (matrix[mid][0] <= target && matrix[mid][MATRIX_WIDTH - 1] >= target) {
+                return hasTarget(matrix[mid], target);
             }
-        }
 
-        int left = 0;
-        int right = matrixWidth - 1;
-
-        if(target > matrix[targetRow][right] || target < matrix[targetRow][left]) return false;
-
-        while(left <= right){
-            int mid = left + (right - left)/2;
-
-            if(matrix[targetRow][mid] == target) return true;
-
-            if(matrix[targetRow][mid] < target){
-                left = mid + 1;
-            }else{
-                right = mid - 1;
+            if (matrix[mid][0] > target) {
+                bottom = mid - 1;
+            } else {
+                top = mid + 1;
             }
         }
 
         return false;
+    }
 
+    public boolean hasTarget(int[] nums, int target) {
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target)
+                return true;
+
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return false;
     }
 }
